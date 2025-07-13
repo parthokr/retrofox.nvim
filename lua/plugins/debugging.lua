91abb1e8414960848ba65e08e0f218ec1663a4e0
parthokr <partho.kr@proton.me>
2025-07-13 13:19:28 +0600

    Add cpp debugger

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -13,9 +13,33 @@ return {
 
 		-- Python Debugger
 		require("dap-python").setup("python3")
-		-- TODO: add Go debugger
+		-- Go debugger
 		require("dap-go").setup()
 		dapui.setup()
+
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+			-- On windows you may have to uncomment this:
+			-- detached = false,
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				cwd = '${workspaceFolder}',
+				stopOnEntry = false,
+			},
+		}
+
+		dap.configurations.c = dap.configurations.cpp
+		dap.configurations.rust = dap.configurations.cpp
+
 
 		dap.listeners.before["event_initialized"]["dapui_config"] = function()
 			dapui.open()
