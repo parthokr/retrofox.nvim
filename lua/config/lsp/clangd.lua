6336b07823cc8452c37ef7b6b311a848724af67e
parthokr <partho.kr@proton.me>
2025-07-14 16:41:03 +0600

    Use fallback-style

--- lua/config/lsp/clangd.lua
+++ lua/config/lsp/clangd.lua
@@ -1,12 +1,15 @@
+local on_attach = require("lsp.utils").on_attach
+
 vim.lsp.config["clangd"] = {
 	-- From the clangd configuration in <rtp>/lsp/clangd.lua
-	cmd = { 'clangd' },
+	cmd = { 'clangd', '--fallback-style=webkit' },
 	-- From the clangd configuration in <rtp>/lsp/clangd.lua
 	-- Overrides the "*" configuration in init.lua
 	root_markers = { '.git', '.clangd', 'compile_commands.json' },
 	-- From the clangd configuration in init.lua
 	-- Overrides the clangd configuration in <rtp>/lsp/clangd.lua
 	filetypes = { 'c', 'cc', 'cpp' },
+	on_attach = on_attach,
 	-- From the "*" configuration in init.lua
 	capabilities = {
 		textDocument = {
