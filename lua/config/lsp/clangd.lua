c8fbef4a62506745ceb13ca9624a3817b65bfa58
parthokr <partho.kr@proton.me>
2025-07-14 19:10:35 +0600

    Remove on_attach

--- lua/config/lsp/clangd.lua
+++ lua/config/lsp/clangd.lua
@@ -1,5 +1,3 @@
-local on_attach = require("lsp.utils").on_attach
-
 vim.lsp.config["clangd"] = {
 	-- From the clangd configuration in <rtp>/lsp/clangd.lua
 	cmd = { 'clangd', '--fallback-style=webkit' },
@@ -9,7 +7,6 @@ vim.lsp.config["clangd"] = {
 	-- From the clangd configuration in init.lua
 	-- Overrides the clangd configuration in <rtp>/lsp/clangd.lua
 	filetypes = { 'c', 'cc', 'cpp' },
-	on_attach = on_attach,
 	-- From the "*" configuration in init.lua
 	capabilities = {
 		textDocument = {
