25f6e015f00ccf271d962306d48eefb2eb579ede
parthokr <partho.kr@proton.me>
2025-08-01 14:46:06 +0600

    Refactored

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -5,19 +5,19 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = home .. '/.local/share/eclipse/' .. project_name
 
 local config = {
-	cmd = {
-		'jdtls',
-		'-data', workspace_dir,
-	},
-	root_dir = require('jdtls.setup').find_root({
-		'.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle',
-	}),
-	settings = {
-		java = {
-			signatureHelp = { enabled = true },
-			contentProvider = { preferred = 'fernflower' },
-		},
-	},
+    cmd = {
+        'jdtls',
+        '-data', workspace_dir,
+    },
+    root_dir = require('jdtls.setup').find_root({
+        '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle',
+    }),
+    settings = {
+        java = {
+            signatureHelp = { enabled = true },
+            contentProvider = { preferred = 'fernflower' },
+        },
+    },
 }
 
 jdtls.start_or_attach(config)
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -31,6 +31,7 @@ return { -- Autoformat
         formatters_by_ft = {
             lua = { "stylua" },
             cpp = { "clang-format" },
+            go = { "gofmt" },
             -- Conform can also run multiple formatters sequentially
             -- python = { "isort", "black" },
             --
