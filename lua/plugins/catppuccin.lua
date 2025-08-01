db7fbf8483858d32c77728aa378ecd5f1898146a
parthokr <partho.kr@proton.me>
2025-08-01 14:45:42 +0600

    Format

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,17 +1,17 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	lazy = false,
-	opts = {
-		integrations = {
-			cmp = true,
-			gitsigns = true,
-			treesitter = true,
-			native_lsp = {
-				enabled = true,
-				semantic_tokens = true,
-			},
-		},
-	},
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            treesitter = true,
+            native_lsp = {
+                enabled = true,
+                semantic_tokens = true,
+            },
+        },
+    },
 }
