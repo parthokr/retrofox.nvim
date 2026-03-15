bb24cfc29ab25107ccd6669e4f7c2f1e58f9e578
parthokr <partho.kr@proton.me>
2026-03-16 01:40:55 +0600

    perf(illuminate): increase delay and disable under_cursor

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -3,11 +3,11 @@ return {
     event = "VeryLazy",
     config = function()
         require("illuminate").configure({
-            providers = { "lsp", "treesitter", "regex" },
-            delay = 100,
+            providers = { "lsp", "treesitter" },
+            delay = 350,
             filetypes_denylist = { "dirbuf", "dirvish", "fugitive" },
             large_file_cutoff = 10000,
-            under_cursor = true,
+            under_cursor = false,
         })
     end,
 }
