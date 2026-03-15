b7cf9791315c4eac022eec0627f1673198d17a6f
parthokr <partho.kr@proton.me>
2026-03-16 01:40:55 +0600

    refactor(flash): update keymaps to use leader key

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,12 +2,11 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
-    -- stylua: ignore
     keys = {
-        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        { "<leader>sj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "[S]earch [J]ump" },
+        { "<leader>st", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "[S]earch [T]reesitter" },
+        { "<leader>sr", mode = "o", function() require("flash").remote() end, desc = "[S]earch [R]emote" },
+        { "<leader>sR", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "[S]earch Treesitter [R]ange" },
+        { "<c-s>", mode = "c", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 }
